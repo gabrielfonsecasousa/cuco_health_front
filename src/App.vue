8012f957fe1506345b96c5f3fd1e95b0dab2c204
“Your <gabrielbromo@outlook.com>
2023-03-07 14:19:34 -0300

    apiRest create/update and filter

--- src/App.vue
+++ src/App.vue
@@ -3,19 +3,27 @@
     <h1>Lista de Clientes</h1>
     <div class="row">
       <div class="col-3 form-group mr-3">
-        <input type="text" class="form-control" placeholder="Nome" />
+        <input
+          type="text"
+          class="form-control"
+          placeholder="Nome"
+          v-model="nameFilter"
+        />
       </div>
       <div class="col-3 form-group mr-3">
         <input
           type="text"
           class="form-control"
           placeholder="CPF"
+          v-model="cpfFilter"
           v-mask="'##.###.###/####-##'"
         />
       </div>
 
       <div class="form-group">
-        <button class="btn btn-primary">Filtrar</button>
+        <button class="btn btn-primary" @click="filterCustomers()">
+          Filtrar
+        </button>
       </div>
     </div>
     <div class="row">
@@ -71,74 +79,89 @@
         </tr>
       </tbody>
     </table>
-    <!-- Create -->
-    <div
-      class="modal fade"
-      id="exampleModalSubmit"
-      tabindex="-1"
-      role="dialog"
-      aria-labelledby="exampleModalLabel"
-      aria-hidden="true"
-    >
-      <div class="modal-dialog" role="document">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLabel">
-              {{ customer ? "Edição" : "Cadastro" }} de Cliente
-            </h5>
-            <button
-              id="closeModal"
-              type="button"
-              class="close"
-              data-dismiss="modal"
-              aria-label="Close"
-            >
-              <span aria-hidden="true">&times;</span>
-            </button>
-          </div>
-          <div class="modal-body">
-            <form @submit.prevent="handleSubmit" style="text-align: left">
-              <div v-if="message" class="alert alert-danger" role="alert">
-                teste
-              </div>
-              <div class="form-group">
-                <label for="name">Name</label>
-                <input type="text" class="form-control" id="name" required />
-              </div>
-              <div class="form-group">
-                <label for="cpf">CPF</label>
-                <input
-                  type="text"
-                  class="form-control"
-                  required
-                  id="cpf"
-                  min="12"
-                />
-              </div>
-              <div class="form-group">
-                <label for="birthday">Aniversario</label>
-                <input
-                  type="date"
-                  required
-                  class="form-control"
-                  id="birthday"
-                />
-              </div>
-              <div class="form-group">
-                <label for="phone">Phone</label>
-                <input type="text" class="form-control" id="phone" />
-              </div>
-              <div class="form-group" style="text-align: right">
-                <button
-                  type="submit"
-                  class="btn btn-success"
-                  style="text-align: right"
-                >
-                  Criar
-                </button>
-              </div>
-            </form>
-          </div>
+  </div>
+  <!-- Create -->
+  <div
+    class="modal fade"
+    id="exampleModalSubmit"
+    tabindex="-1"
+    role="dialog"
+    aria-labelledby="exampleModalLabel"
+    aria-hidden="true"
+  >
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">
+            {{ customer ? "Edição" : "Cadastro" }} de Cliente
+          </h5>
+          <button
+            id="closeModal"
+            type="button"
+            class="close"
+            data-dismiss="modal"
+            aria-label="Close"
+          >
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <form @submit.prevent="handleSubmit" style="text-align: left">
+            <div v-if="message" class="alert alert-danger" role="alert">
+              {{ message }}
+            </div>
+            <div class="form-group">
+              <label for="name">Name</label>
+              <input
+                type="text"
+                class="form-control"
+                id="name"
+                required
+                v-model="form.name"
+              />
+            </div>
+            <div class="form-group">
+              <label for="cpf">CPF</label>
+              <input
+                type="text"
+                class="form-control"
+                required
+                :onkeyup="maskCpf"
+                id="cpf"
+                min="12"
+                v-model="form.cpf"
+              />
+            </div>
+            <div class="form-group">
+              <label for="birthday">Aniversario</label>
+              <input
+                type="date"
+                required
+                class="form-control"
+                id="birthday"
+                v-model="form.birthday"
+              />
+            </div>
+            <div class="form-group">
+              <label for="phone">Phone</label>
+              <input
+                type="text"
+                class="form-control"
+                id="phone"
+                :onkeyup="maskPhone"
+                v-model="form.phone"
+              />
+            </div>
+            <div class="form-group" style="text-align: right">
+              <button
+                type="submit"
+                class="btn btn-success"
+                style="text-align: right"
+              >
+                {{ form.id ? "Editar" : "Criar" }}
+              </button>
+            </div>
+          </form>
         </div>
       </div>
     </div>
@@ -189,8 +212,9 @@
   </div>
 </template>
 
-<script>
+<script type="text/javascript">
 import axios from "axios";
+
 export default {
   data() {
     return {
@@ -208,38 +232,19 @@ export default {
     this.getCustomers();
   },
   methods: {
-    getCustomers() {
-      axios
-        .get("http://127.0.0.1:8000/api/customers")
-        .then((response) => {
-          this.customers = response.data["data"];
-        })
-        .catch((error) => {
-          console.log(error);
-        });
+    maskCpf(event) {
+      const cpf = event.target.value
+        .replace(/\D/g, "") // remove caracteres não numéricos
+        .replace(/(\d{3})(\d)/, "$1.$2") // coloca um ponto entre o terceiro e o quarto dígitos
+        .replace(/(\d{3})(\d)/, "$1.$2") // coloca um ponto entre o sexto e o sétimo dígitos
+        .replace(/(\d{3})(\d{1,2})/, "$1-$2"); // coloca um hífen entre o nono e o décimo dígitos (se houver)
+      event.target.value = cpf;
     },
-    deleteCustomer(customer) {
-      console.log(customer);
-      axios
-        .delete("http://127.0.0.1:8000/api/customers/" + customer.id)
-        .then(() => {
-          this.getCustomers();
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    },
-    showCustomer(customer) {
-      console.log(customer != null);
-      var customerObject = {
-        id: customer != null ? customer.id : "",
-        cpf: customer != null ? customer.cpf : "",
-        name: customer != null ? customer.name : "",
-        birthday:
-          customer != null ? this.dateFormatInput(customer.birthday) : "",
-        phone: customer != null ? customer.phone : "",
-      };
-      this.customer = customerObject;
+    maskPhone(event) {
+      let phone = event.target.value.replace(/\D/g, ""); // remove all non-numeric characters
+      phone = phone.replace(/^(\d{2})(\d)/g, "($1) $2"); // add parentheses and space after the first two digits
+      phone = phone.replace(/(\d{4})(\d)/, "$1-$2"); // add hyphen after the next four digits
+      event.target.value = phone;
     },
     dateFormat(date) {
       let dateString = date;
@@ -261,18 +266,79 @@ export default {
       let formattedDate = `${year}-${month}-${day}`;
       return formattedDate;
     },
+    filterCustomers() {
+      axios
+        .get(
+          `http://127.0.0.1:8000/api/customers?cpf=${this.cpfFilter}&name=${this.nameFilter}`
+        )
+        .then((response) => {
+          this.customers = response.data["data"];
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+    getCustomers() {
+      axios
+        .get("http://127.0.0.1:8000/api/customers")
+        .then((response) => {
+          this.customers = response.data["data"];
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+    deleteCustomer(customer) {
+      axios
+        .delete("http://127.0.0.1:8000/api/customers/" + customer.id)
+        .then(() => {
+          this.getCustomers();
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+    showCustomer(customer) {
+      var customerObject = {
+        id: customer ? customer.id : "",
+        cpf: customer ? customer.cpf : "",
+        name: customer ? customer.name : "",
+        birthday: customer ? this.dateFormatInput(customer.birthday) : "",
+        phone: customer ? customer.phone : "",
+      };
+      this.customer = customerObject;
+      this.form = customerObject;
+    },
+    onDelete(customer) {
+      axios
+        .delete("http://127.0.0.1:8000/api/customers/" + customer.id)
+        .then(() => {
+          this.getCustomers();
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+    async handleSubmit() {
+      try {
+        this.form.id
+          ? await axios.put(
+              `http://127.0.0.1:8000/api/customers/${this.form.id}`,
+              this.form
+            )
+          : await axios.post("http://127.0.0.1:8000/api/customers", this.form);
+        this.getCustomers();
+        const closeModal = document.querySelector("#closeModal");
+        closeModal.click();
+        this.message = "";
+      } catch (error) {
+        this.message = error.response.data.message;
+      }
+    },
+    onCancel() {
+      this.showForm = false;
+    },
   },
-  components: {},
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+ 
