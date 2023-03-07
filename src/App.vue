fa01acd5eb637b3ba55f66436da53402cd91adc7
“Your <gabrielbromo@outlook.com>
2023-03-07 11:48:31 -0300

    layout listagem

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,53 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <h1>Lista de Clientes</h1>
+    <table class="table mt-2">
+      <thead>
+        <tr>
+          <th>Nome</th>
+          <th>CPF</th>
+          <th>Data de Nascimento</th>
+          <th>Telefone</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>test</td>
+          <td>teste</td>
+          <td>test</td>
+          <td>test</td>
+          <td>
+            <div class="btn-group mr-2">
+              <button
+                class="btn btn-success"
+                data-toggle="modal"
+                data-target="#exampleModalSubmit"
+              >
+                Editar
+              </button>
+            </div>
+            <div class="btn-group mr-2">
+              <button
+                class="btn btn-danger"
+                data-toggle="modal"
+                data-target="#exampleModal"
+              >
+                Deletar
+              </button>
+            </div>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: {},
+};
 </script>
 
 <style>
